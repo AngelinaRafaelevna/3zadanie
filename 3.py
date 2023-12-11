e02c99398edb389f3a33caaee7b4f7bf55bf7b74
tjatik1 <128229582+tjatik1@users.noreply.github.com>
2023-12-11 20:03:29 +0300

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,68 @@
+#true = True
+#if true==True:
+   # print(f"{true}: True")
+   # print(type(true))
+    
+   # print("hello there")
+    
+   # print("1==1:", 1==1)  #1==1:True
+    #print("1==2:", 1==2)   #1==2:False
+   # print("1!=1:", 1!=1)   #1!=1:False
+   # print("1!=2:", 1!=2)  #1!=2:True
+   # print("1>1:", 1>1)   #1>1:False
+   # print("1>2:", 1>2)   #1>2:False
+    
+    #competent=True
+   # responsible=True
+    #print(competent and responsible)
+    
+    #competent=True
+    #responsible=True
+   # print(competent or responsible)
+    
+   # previosly_fired=True
+   # print(not previosly_fired)
+    
+    #speed = int(input("please enter your speed:"))
+   #if speed>200:
+   #     print("it's very fast")
+   #elif 100<speed<200
+   #if speed ==160:
+    #   print("u folow the recommedation")
+   #    print("it's common speed")
+   #else:
+   #     print("u are too slow")
+   
+   #задание1
+number=int(input("number:"))
+if number %2==0:
+    print("even number")
+else:
+    print("odd number")
+       
+       #задание2
+if number %7==0:
+    print("number is multiole 7:")
+else:
+    print("number is not multiple 7:")
+    
+    #задание3
+a, b = int(input("number1:")), int(input("number2:"))
+if a > b:
+    print(a)
+else:
+    print(b)   
+       
+        #задание4
+a, b = int(input("number1:")), int(input("number2:"))
+if a < b:
+    print(a)
+else:
+    print(b) 
+    
+      #задание5
+first_num=int(input("enter the first number:"))
+second_num=int(input("enter the second number:"))
+print(f"сумма чисел: {first_num+second_num}")
+print(f"разность чисел: {first_num*second_num}")
+print(f"произведение чисел: {first_num//second_num}")
